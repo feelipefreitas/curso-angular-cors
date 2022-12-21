bb8bc3f319d9d89826561327dab8980190e334c0
Felipe Freitas <feelipe.freitas97@gmail.com>
2022-12-21 08:38:43 -0300

    Removendo implementação anterior

--- index.js
+++ index.js
@@ -4,40 +4,6 @@ const server = express();
 
 server.use(express.json());
 
-const validOrigins = [
-    {
-        origin: 'http://127.0.0.1:8080',
-        methods: 'PUT',
-        headers: 'content-type,x-auth-custom',
-        credentials: true,
-    },
-    {
-        origin: 'http://127.0.0.1:5050',
-        methods: 'PUT,POST,DELETE',
-        headers: 'content-type,x-auth-custom',
-        credentials: false,
-    },
-];
-
-server.use((req, res, next) => {
-    if(req.method === 'OPTIONS') {
-        console.log('OPTIONS - Pre-Flight');
-
-        const validOrigin = validOrigins.find(validOrigin => validOrigin.origin === req.get('Origin'));
-
-        if(validOrigin) {
-            res.setHeader('Access-Control-Allow-Origin', validOrigin.origin);
-            res.setHeader('Access-Control-Allow-Methods', validOrigin.methods);
-            res.setHeader('Access-Control-Allow-Headers', validOrigin.headers);
-            res.setHeader('Access-Control-Allow-Credentials', validOrigin.credentials);
-        }
-
-        return res.status(204).send();
-    }
-
-    next();
-});
-
 const users = [
     {
         id: 0,
@@ -47,14 +13,6 @@ const users = [
 ];
 
 server.put('/users/:id', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', req.get('Origin'));
-
-    const validOrigin = validOrigins.find(validOrigin => validOrigin.origin === req.get('Origin'));
-
-    if(validOrigin) {
-        res.setHeader('Access-Control-Allow-Credentials', validOrigin.credentials);
-    }
-
     const id = +req.params.id;
     const newUser = req.body;
 
